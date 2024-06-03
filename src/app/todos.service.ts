37cd7735b1e06d279e599e7a2d70013db9eaea86
Valeria Vera <valeria2251997@gmail.com>
2024-06-03 18:56:32 -0300

    feat: se agrega interfaz

--- src/app/todos.service.ts
+++ src/app/todos.service.ts
@@ -1,6 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+export interface Todo {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 @Injectable({
   providedIn: 'root',
 })
